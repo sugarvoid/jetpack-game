9052b73e7a3dd869b07814706e12b70f33dc38b8
Dave <gamedev@kitargo.com>
2022-11-25 16:58:08 -0600

    Create new gun png and make player sprite update

--- game/actor/player/player.gd
+++ game/actor/player/player.gd
@@ -7,13 +7,40 @@ const JUMP_VELOCITY = -40.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_facing_right: bool = true
+
+func _update_facing_dir(target: Vector2):
+
+	if target.x < 0:
+		if is_facing_right:
+			scale.x = -1
+			is_facing_right = false
+	else: 
+		if not is_facing_right:
+			scale.x = 1
+			is_facing_right = true
+
+
+
+#	#Horizontal flip
+#	if target.x > self.global_position.x && is_facing_right == false:
+#		self.scale.x = 1
+#		self.is_facing_right = true
+#
+#	if target.x < self.global_position.x && is_facing_right == true:
+#		self.scale.x = -1
+#		self.is_facing_right = false
 
 
 func _physics_process(delta: float) -> void:
-
+	
+	var mouse_location: Vector2 = get_local_mouse_position()
+	self._update_facing_dir(mouse_location)
+	
+	$Marker2D/agun.look_at(get_global_mouse_position())
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y = min(velocity.y + 6, gravity) 
+		velocity.y = min(velocity.y + 3, gravity) 
 		#velocity.y += gravity * delta
 
 
@@ -26,7 +53,7 @@ func _physics_process(delta: float) -> void:
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
