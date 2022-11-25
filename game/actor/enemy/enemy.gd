6bc967fd8c91e8b1889aa30e520ee83e59b74b18
Dave <gamedev@kitargo.com>
2022-11-25 14:38:21 -0600

    Init commit

---
+++ game/actor/enemy/enemy.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -40.0
+
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+	
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y = min(velocity.y + 6, gravity) 
+		velocity.y += gravity * delta
+	move_and_slide()
---
+++ game/actor/player/player.gd
@@ -0,0 +1,35 @@
+class_name Player
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -40.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta: float) -> void:
+
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y = min(velocity.y + 6, gravity) 
+		#velocity.y += gravity * delta
+
+
+
+	# Handle Jump.
+	if Input.is_action_pressed("jump"): # and is_on_floor():
+		velocity.y = min(velocity.y - 2, JUMP_VELOCITY)
+	if Input.is_action_just_pressed("shoot"):
+		print('bang')
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
