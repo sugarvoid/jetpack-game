02a53b1ad3a71fa0ad8a08248e093598ee84a85f
Dave <48969490+sugarvoid@users.noreply.github.com>
2022-12-11 09:34:56 -0600

    Clean up hud.gd

--- game/hud/hud.gd
+++ game/hud/hud.gd
@@ -11,40 +11,34 @@ const GREEN_REGION: Rect2 = Rect2(0,0,8,8)
 
 var heat_high_value: float
 
-func _ready() -> void:
-	print(pgb_player_heat.max_value)
-	print(self.heat_high_value)
-	
 
 func update_heat_high_value() -> void:
 	var bar_max = self.pgb_player_heat.max_value
 	self.heat_high_value = _get_percent_of_float(bar_max, 80.0)
 
 func _process(delta: float) -> void:
-	print(pgb_player_heat.max_value)
+	_update_heat_bar_color()
+
+func _update_heat_bar_color() -> void:
 	if pgb_player_heat.value >= self.heat_high_value:
 		_change_heat_bar_color(RED_REGION)
 	else:
 		_change_heat_bar_color(GREEN_REGION)
 
-func _get_percent_of_float(f: float, percent: float) -> float:
-	return (percent / 100) * f
+func _get_percent_of_float(value: float, percent: float) -> float:
+	return (percent / 100) * value
 
 func _change_heat_bar_color(region: Rect2) -> void:
 	self.pgb_player_heat.texture_progress.set_region(region)
 
+func update_score(value: int) -> void:
+	self.lbl_score.text = str("Score: ", ("%03d" % value))
 
-func update_score(num: int) -> void:
-	self.lbl_score.text = str("Score: ", ("%03d" % num))
+func update_level(value: int) -> void:
+	self.lbl_level.text = str("Level: ", ("%03d" % value))
 
+func update_heat_bar_max_value(value: float) -> void:
+	self.pgb_player_heat.max_value = int(value)
 
-func update_level(num: int) -> void:
-	self.lbl_level.text = str("Level: ", ("%03d" % num))
-
-
-func update_heat_bar_max_value(n: float) -> void:
-	self.pgb_player_heat.max_value = int(n)
-
-
-func update_jetpack_heat_bar(amount: float) -> void:
-	self.pgb_player_heat.value = amount
+func update_jetpack_heat_bar(value: float) -> void:
+	self.pgb_player_heat.value = value
